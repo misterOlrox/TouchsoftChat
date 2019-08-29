dbaf463b12976377b51eb65fcfb51451db95a60e
Nikita <nikitakholdobin@yandex.ru>
2019-08-29 17:20:15 +0300

    Fix NPE in deliverMessage()

--- server-app/src/main/java/com/olrox/chat/server/ChatRoom.java
+++ server-app/src/main/java/com/olrox/chat/server/ChatRoom.java
@@ -1,10 +1,11 @@
 package com.olrox.chat.server;
 
-import com.olrox.chat.server.user.Agent;
-import com.olrox.chat.server.user.Client;
-import com.olrox.chat.server.user.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ChatRoom {
+
+    private final static Logger logger = LogManager.getLogger(ChatRoom.class);
 
     private UserThread clientThread;
     private UserThread agentThread;
@@ -27,13 +28,25 @@ public class ChatRoom {
 
     public void deliverMessage(String text, UserThread from) {
         // TODO saving
-        // FIXME NPE
+        if(agentThread==null || clientThread==null) {
+            noCompanionMessage(from);
+            return;
+        }
+
         if(agentThread==from) {
+            logger.debug("Agent " + agentThread.getUser().getUsername() +" writes message: " + text);
+            logger.debug("Client " + clientThread.getUser().getUsername() + " receives message: " + text);
             clientThread.getMessage(text);
         }
         else {
+            logger.debug("Client " + clientThread.getUser().getUsername() +" writes message: " + text);
+            logger.debug("Agent " + agentThread.getUser().getUsername() + " receives message: " + text);
             agentThread.getMessage(text);
         }
 
+    }
+
+    public void noCompanionMessage(UserThread userThread) {
+        userThread.getMessage("Wait for your companion");
     }
 }
--- server-app/src/main/java/com/olrox/chat/server/Server.java
+++ server-app/src/main/java/com/olrox/chat/server/Server.java
@@ -1,15 +1,15 @@
 package com.olrox.chat.server;
 
-import com.olrox.chat.server.user.Agent;
-import com.olrox.chat.server.user.Client;
-import com.olrox.chat.server.user.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
 
 public class Server {
 
@@ -75,84 +75,4 @@ public class Server {
     public void addRoomToFreeAgents(ChatRoom room) {
         freeAgents.add(room);
     }
-
-//    public ChatRoom findChatRoom(User user){
-//        if(user.getChatRoom()!=null){
-//            return user.getChatRoom();
-//        }
-//
-//        switch(user.getRole()){
-//            case AGENT:
-//                if(!this.hasFreeClient()){
-//                    freeAgents.add(user);
-//                    user.setChatRoom(new ChatRoom(user));
-//                    return user.getChatRoom();
-//                }
-//                else{
-//                    User mate = freeClients.poll();
-//
-//                }
-//                break;
-//            case CLIENT:
-//                if(!this.hasFreeAgent()) {
-//                    freeClients.add(user);
-//                    return null;
-//                }
-//                else{
-//                    return freeAgents.poll();
-//                }
-//                break;
-//        }
-//    }
-//
-//    public void getFreeAgent(User client){
-//        while(!freeAgents.isEmpty()){
-//            User freeAgent = freeAgents.poll();
-//            if(freeAgent.isOnline()){
-//                // TODO finish this
-//                return;
-//            }
-//        }
-//        freeClients.add(client);
-//    }
-//
-//    /**
-//     * Delivers a message from one user to others (broadcasting)
-//     */
-//    void broadcast(String message, UserThread excludeUser) {
-//        for (UserThread aUser : userThreads) {
-//            if (aUser != excludeUser) {
-//                aUser.sendMessage(message);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Stores username of the newly connected client.
-//     */
-//    void addUserName(String userName) {
-//        usernames.add(userName);
-//    }
-//
-//    /**
-//     * When a client is disconneted, removes the associated username and UserThread
-//     */
-//    void removeUser(String userName, UserThread aUser) {
-//        boolean removed = usernames.remove(userName);
-//        if (removed) {
-//            userThreads.remove(aUser);
-//            System.out.println("The user " + userName + " quitted");
-//        }
-//    }
-//
-//    Set<String> getUsernames() {
-//        return this.usernames;
-//    }
-//
-//    /**
-//     * Returns true if there are other users connected (not count the currently connected user)
-//     */
-//    boolean hasUsers() {
-//        return !this.usernames.isEmpty();
-//    }
 }
